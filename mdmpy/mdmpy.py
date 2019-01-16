5b896f268be6d59ac7dae8a14565636c18f18ebc
Mona Lisa <3600019+justanothergithubber@users.noreply.github.com>
2019-01-16 14:44:11 +0800

    Taking shape

--- mdmpy/mdmpy.py
+++ mdmpy/mdmpy.py
@@ -1,6 +1,11 @@
+import util
+
 class mdm:
     def __init__(self):
         self.X = 3.5
 
     def class_test_function():
         print("This function is for testing")
+
+    def second_test():
+        return useful_func()
---
+++ mdmpy/util.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+def useful_func():
+    return np.random.random(10)
