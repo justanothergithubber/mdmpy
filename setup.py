d58f8b3b76ab2f8bf6a7300652fb4d3239802e2f
Mona Lisa <3600019+justanothergithubber@users.noreply.github.com>
2019-02-08 16:24:35 +0800

    Unbreaking setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,8 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/justanothergithubber/mdmpy",
-    packages=setuptools.find_packages(),
+    packages=["mdmpy"],
+    package_dir={'':'src'},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
